4d5ae4c9df9b982ba4a291a2ae7d3feab6fdc7a5
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2025-02-16 13:08:51 +0530

    updated cors

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ import pool, { initDB } from "./db.js";
 
 const app = express();
 
+const whitelist = [process.env.CLIENT_URL];
 const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  optionsSuccessStatus: 200,
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
 };
 
 app.use(cors(corsOptions));
